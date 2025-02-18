6e0a309bae195d3d0064e241880aeb47e579d572
Estudiante <Estudiante@DESKTOP-TITVKIH>
2025-02-18 14:23:58 -0300

    Agregar archivos de proyecto.

---
+++ ilias tan puto que trasciende la homosexualidad/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ilias_tan_puto_que_trasciende_la_homosexualidad
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = -123;
+            Reverse(n);
+        }
+
+        public static int Reverse(int x)
+        {
+            Stack<char> numero = new Stack<char>();
+            string numeros = x.ToString();
+            int cantidad = numeros.Length;
+            bool esNegativo = numeros[0] == '-';
+
+            for (int i = (esNegativo ? 1 : 0); i < cantidad; i++)
+            {
+                numero.Push(numeros[i]); // Agrega cada dígito al stack
+            }
+
+            string numeroInvertido = (esNegativo ? "-" : "") + string.Concat(numero);
+
+            int resultado = int.Parse(numeroInvertido);
+
+            return resultado;
+        }
+
+    }
+}
+
